317cd392d52bb546a62f4839afd53156648b4658
DominicSina <dominic.sina@gmx.ch>
2015-10-17 02:08:13 +0200

    User is now able to change their profile data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,5 +9,20 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user=User.find(params[:id])
   end
+
+  def update
+    @user=User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user, notice: 'User profile was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :password, :email)
+    end
 end
