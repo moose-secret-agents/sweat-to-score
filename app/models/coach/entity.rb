c638a692674773a2f6d6b1e696b983b6e6990a3e
Urs Gerber <ug.gerber@gmail.com>
2015-10-17 17:53:51 +0200

    All users can now be retrieved from cybercoach server using "Coach::User.all"

--- app/models/coach/entity.rb
+++ app/models/coach/entity.rb
@@ -3,13 +3,27 @@ require 'ostruct'
 module Coach
   class Entity < OpenStruct # < ActiveRestClient::Base
     include HTTParty
-    # Use JSON parser in response
-    #format :json
-    # Request JSON response from server
+
     headers  'Accept' => 'application/json'
-
     debug_output $stdout
-
     base_uri 'http://diufvm31.unifr.ch:8090/CyberCoachServer/resources'
+
+    # Fetch an entity based on its uri
+    def fetch
+      return self if @fetched
+
+      raise 'Entity has not been loaded yet and does not have "uri" property' if self.uri.blank?
+
+      response = Entity.get clean_uri
+      instance = self.class.new(JSON.parse(response.body))
+
+      @fetched = true
+
+      instance
+    end
+
+    def clean_uri
+      self.uri.gsub('/CyberCoachServer/resources', '')
+    end
   end
 end
--- app/models/coach/user.rb
+++ app/models/coach/user.rb
@@ -1,32 +1,36 @@
 module Coach
   class User < Coach::Entity
-    def self.all
-      get '/users'
+
+    # Find all users on server. Set eager to true if you want all user attributes to be fetched from the beginning (slow)
+    def self.all(eager=false)
+      response = get '/users'
+      JSON.parse(response.body)['users'].map { |u| eager ? Coach::User.new(u).fetch : Coach::User.new(u) }
     end
 
+    # Find user by username
     def self.find(username, password=nil)
       auth = { username: username, password: password }
       response = get "/users/#{username}", basic_auth: auth
-      if response.code == 200
-        Coach::User.new(JSON.parse(response.body))
-      else
-        nil
-      end
+      response.code == 200 ? Coach::User.new(JSON.parse(response.body)) : nil
     end
 
+    # Check whether user exists
     def self.exists?(username)
       !!find(username)
     end
 
+    # Create new user
     def self.create(username, attributes={})
       put "/users/#{username}", body: attributes
     end
 
+    # Update user
     def self.update(username, password, attributes={})
       auth = { username: username, password: password }
       put "/users/#{username}", body: attributes, basic_auth: auth
     end
 
+    # Delete user
     def self.delete(username, password)
       auth = { username: username, password: password }
       HTTParty::delete "/users/#{username}", basic_auth: auth
