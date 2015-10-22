a43fab519be38ba35c636ede4078112d8dbedc09
Mansour Hamidi <mansourhamidi@hotmail.com>
2015-10-22 23:48:21 +0200

    added ranking and view to teams in leagues

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151022113351) do
+ActiveRecord::Schema.define(version: 20151022165730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20151022113351) do
     t.integer  "league_id"
     t.integer  "teamable_id"
     t.string   "teamable_type"
+    t.integer  "points"
   end
 
   add_index "teams", ["league_id"], name: "index_teams_on_league_id", using: :btree
