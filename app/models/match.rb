c5d47e2e5f4076ca4d234bcff1543adbc91108dc
Mansour Hamidi <mansourhamidi@hotmail.com>
2015-11-09 18:32:04 +0100

    modified match table views with timeline

--- app/models/match.rb
+++ app/models/match.rb
@@ -5,4 +5,29 @@ class Match < ActiveRecord::Base
   belongs_to :teamB, class_name: 'Team'
 
   enum status: { scheduled: 0, started: 1, ended: 2, cancelled: 3 }
+
+  def glyphicon
+    case status
+      when 'scheduled'
+        "glyphicon-pencil"
+      when 'started'
+        "glyphicon-ok"
+      when 'ended'
+        "glyphicon-flag"
+      when 'cancelled'
+        "glyphicon-remove"
+    end
+  end
+
+  def getTimeDifference(time)
+    delta = time - Time.now
+    t = %w[days hours minutes]
+
+    t.collect do |step|
+      seconds = 1.send(step)
+      (delta / seconds).to_i.tap do
+        delta %= seconds
+      end
+    end
+  end
 end
