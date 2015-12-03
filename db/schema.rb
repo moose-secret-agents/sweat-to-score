78f51a879f572e006f1f9eda5f7ab746c29b213f
Marian Briceag <mbriceag@gmail.com>
2015-12-03 14:49:22 +0100

    Add target for Leagues

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151119130808) do
+ActiveRecord::Schema.define(version: 20151203133034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20151119130808) do
     t.datetime "starts_at"
     t.integer  "league_length"
     t.integer  "pause_length"
+    t.integer  "target"
   end
 
   add_index "leagues", ["owner_id"], name: "index_leagues_on_owner_id", using: :btree
