030d14abc59c8d818d27ce5064872354a196ede5
DominicSina <dominic.sina@gmx.ch>
2015-12-09 04:02:49 +0100

    added default values in league form, recent matches and signups in news page

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,4 +1,7 @@
 class NewsController < ApplicationController
   def index
+    @matches=Match.all.order(starts_at: :desc).limit(5)
+    @users=User.all.order(created_at: :desc).limit(5)
+    #@matches=Match.ended.order(starts_at: :desc).limit(5)
   end
 end
