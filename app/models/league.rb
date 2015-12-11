98ee3c21dccfc26c76b5920f280401710e4112ec
DominicSina <dominic.sina@gmx.ch>
2015-12-11 17:48:12 +0100

    validates pause_length

--- app/models/league.rb
+++ app/models/league.rb
@@ -5,7 +5,7 @@ class League < ActiveRecord::Base
 
   belongs_to :owner, class_name: 'User'
 
-  validates_presence_of :name, :target
+  validates_presence_of :name, :target, :pause_length
 
   scope :overdue, -> { League.inactive.where('starts_at < ?', Time.now) }
   scope :should_finish, -> {League.active-League.active.includes(:matches).where(:matches=>{status: [Match.statuses[:running], Match.statuses[:scheduled]]})}
