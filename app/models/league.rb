8979384cdaa214c76084dc16e15329c3d55b9063
DominicSina <dominic.sina@gmx.ch>
2015-12-11 16:39:22 +0100

    tweaked league start and end tweets

--- app/models/league.rb
+++ app/models/league.rb
@@ -22,14 +22,14 @@ class League < ActiveRecord::Base
       sched.schedule_season(self)
       active!
       update! starts_at: starts_at+sched.actual_league_length(self).days+pause_length.days
-      Twitterer.new.tweet(" The league #{(self.name)} has been started.")
+      Twitterer.new.tweet(" The #{(self.name)} has been started.")
     end
   end
 
   def end
     Match.destroy_all(:status=> Match.statuses[:scheduled],:league=>self)
     inactive!
-    Twitterer.new.tweet(" The league #{(self.name)} has been ended.")
+    Twitterer.new.tweet(" The #{(self.name)} has been ended.")
   end
 
   def schedule_match(team_a, team_b, time=1.day.from_now)
